de2172f69653266b4d231af699a9ce833e0c7c69
hortenbach <gina.hortenbach@hhi.fraunhofer.de>
2020-04-24 19:47:10 +0200

    added write call

--- Aufgabe1/aufgabe1.c
+++ Aufgabe1/aufgabe1.c
@@ -4,15 +4,15 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-
 int main(){
 
-    char buffer;
+    int n = 0;
+    char buffer[3000];
     int irq_fd = open("/proc/interrupts", O_RDONLY);
 
-    while (read(irq_fd, &buffer, 1) > 0){
-        printf("%c", buffer);
+    while (read(irq_fd, &buffer[n++], 1) > 0){
     }
+    write(STDOUT_FILENO, &buffer[0], sizeof(buffer)/sizeof(char));
     close(irq_fd);
 
     return 0;
