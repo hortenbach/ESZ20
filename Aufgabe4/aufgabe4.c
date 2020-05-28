0e921e26538250907a4b041f2c4c56552c507b59
hortenbach <gina.hortenbach@hhi.fraunhofer.de>
2020-05-28 21:21:26 +0200

    priority error

--- Aufgabe4/aufgabe4.c
+++ Aufgabe4/aufgabe4.c
@@ -14,45 +14,45 @@
 double n_4(int);
 void * a3(void *vargp);
 
-
 int main(int argc, char *argv[])
 {    
     if (argc > 2){
         fprintf(stderr, "Too many Arguments!\nUsage: aufgabe4 [RT priority (-19 to 19)]\nUser might add no or one optional argument.\n");
         exit(EXIT_FAILURE);
     }
-    
-    printf("DEBUG command line: %s %s\n", argv[0], argv[1]);
 
-    /*
-     * aufgabe4
-     * */
     pthread_attr_t tattr;
     pthread_t tid;
     pid_t pid = getpid();
+    /* choose real time policy FIFO */
     int policy = SCHED_FIFO;
     int prio;
     int ret, rc;
     struct sched_param param;
 
-
     /* initialized with default attributes */
-    ret = pthread_attr_init (&tattr);
+    pthread_attr_init (&tattr);
 
     /* safe to get existing scheduling param */
-    ret = pthread_attr_getschedparam (&tattr, &param);
+    pthread_attr_getschedparam (&tattr, &param);
 
     if(argc == 2){
         prio = atoi(argv[1]);
-        prio = -10;
         if((prio > (-19)) && (prio < 19)){
             /* set the priority; others are unchanged */
             param.sched_priority = prio;
+            printf("Priority: %d\n", param.sched_priority);
+        } else {
+            printf("Ignored invalid priority.\n");
         }
     }
 
     /* setting the new scheduling param */
     ret = pthread_attr_setschedparam (&tattr, &param);
+    if(ret != 0){
+        fprintf(stderr, "Couldnt set priority!\n");
+        exit(EXIT_FAILURE);
+    }
 
     //pthread_create(&tid, &tattr, &a3, NULL);
     rc = pthread_create(&tid, NULL, a3, NULL);
@@ -62,28 +62,18 @@ int main(int argc, char *argv[])
         printf("\n ERROR: return code from pthread_create is %d \n", rc);
         exit(1);
     }
-    printf("\n Created new thread (%u) ... \n", tid);
-    /*****/
-
-    const struct timespec req =  { .tv_sec = 1, .tv_nsec = 0 } ;
-    struct timespec rem;
-    nanosleep(&req,&rem);
+    pthread_join(tid, NULL);            /* wait for thread tid     */
     
     return 0;
 }
 
 void * a3(void *vargp){
 
-    const int FAK = 31; // Faktor, der n erhöht für die n^4 Berechnung (24 bei Urs)
+    const int FAK = 16; // Faktor, der n erhöht für die n^4 Berechnung (24 bei Urs)
     double res = 0.0;
     struct timeval start,end;
     unsigned long seconds, microseconds;
     char line[80]={' '};
-
-    pthread_t tid = (pthread_t)vargp;    /* data received by thread */
-    pthread_join(tid, NULL);            /* wait for thread tid     */
-
-//    pthread_detach(pthread_self());
 
     // Ausgabe Titelzeile
     sprintf(line," n%18sn_4%2sdelta_t[s]\n\r","","");
